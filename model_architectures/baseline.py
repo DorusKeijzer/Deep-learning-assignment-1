b533a40d79d1bf7c4cdaa7d322e27731e9881e39
Dorus <doruskeijzer@gmail.com>
2025-04-30 13:21:40 +0200

    fix: made baselines work with batches

--- model_architectures/baseline.py
+++ model_architectures/baseline.py
@@ -5,11 +5,11 @@ class MostRecentBaseline(torch.nn.Module):
     Baseline model that returns the most recent entry in the feature.
     """
     def forward(self, x):
-        return x[-1]
+        return x[:,-1]
 
 class MeanBaseline(torch.nn.Module):
     """
     Baseline model that returns the mean of the feature.
     """
     def forward(self, x):
-        return torch.mean(x)
+        return torch.mean(x, dim=1)
