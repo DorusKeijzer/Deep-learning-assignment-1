0bb7b2aefeaa0d9704a8374dec69948e0baa5c39
Dorus <doruskeijzer@gmail.com>
2025-04-30 11:42:13 +0200

    wrote dataloader

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,5 +1,28 @@
 from scipy.io import loadmat
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+import numpy as np
 
-data = loadmat("data/Xtrain.mat")
+class LaserDataset(Dataset):
+    def __init__(self, file_path: str, lag_size: int):
+        self.data = np.array(loadmat(file_path)["Xtrain"])
+        self.data = np.append(np.zeros(lag_size), self.data)
+        self.lag_size = lag_size
 
-print(data)
+    def __len__(self): 
+        return len(self.data)
+    def __getitem__(self, idx: int):
+        target = self.data[idx+1+self.lag_size]
+        feature = self.data[idx: idx+self.lag_size]
+
+        return feature, target
+
+if __name__ == "__main__":
+    ds = LaserDataset("data/Xtrain.mat", 3)
+    for i, d in ds:
+        print(i, d)
+
+
